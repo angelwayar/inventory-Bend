0e7bb80bf8f798ae7fd0dee607b0bb4a6fa7be32
angelwayar <angelmanuelwayar@gmail.com>
2024-03-16 09:33:51 -0400

    update model

--- app/models/product.py
+++ app/models/product.py
@@ -1,6 +1,10 @@
+import base64
+
 from typing import List, Optional
 
 from pydantic import BaseModel
+
+PATH = "../assets/images/"
 
 
 class Product(BaseModel):
@@ -16,14 +20,39 @@ class Product(BaseModel):
     images: Optional[List[str]] = None
     brand: Optional[str] = None
 
+    def get_image_product(path: str | None):
+        try:
+            if path is not None:
+                with open(path, 'rb') as image_file:
+                    image_bytes = image_file.read()
+
+                image_base64 = base64.b64encode(image_bytes)
+
+                return image_base64.decode("utf-8")
+
+            return ''
+        except Exception as _e:
+            print(f"Error al leer la imagen: {path}")
+            return ''
+
     @classmethod
     def from_document(cls, document: dict) -> "Product":
         document["code"] = str(document.get("code", ""))
         document["year"] = str(document.get("year", ""))
+        image_array = []
+        images = document.get("images", None)
+
         _id = document.get("_id", None)
-        
+
         if _id:
             document["id"] = str(_id)
+
+        if images and len(images) > 0:
+            for image in images:
+                imagebase64 = cls.get_image_product(path=image)
+                image_array.append(imagebase64)
+
+            document["images"] = image_array
 
         return cls(**document)
 
